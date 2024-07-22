325745da5286bfed7b8bad60ae55da8d360e841b
Tejas Mahajan <114793178+Tejas-Mahajan1@users.noreply.github.com>
2024-07-22 13:23:05 +0530

    Auth Changes

--- app/(auth)/sign-up/[[...sign-up]]/page.tsx
+++ app/(auth)/sign-up/[[...sign-up]]/page.tsx
@@ -2,7 +2,7 @@ import { SignUp } from "@clerk/nextjs";
 
 export default function Page() {
   return (
-    <center className="my-48">
+    <center className="my-40">
       <SignUp />;
     </center>
   )
